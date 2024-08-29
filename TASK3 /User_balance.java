f376968b4929885f62c796887fb8551454fb18d1
abhinandan6123 <abhikancherla0@gmail.com>
2024-08-29 23:11:47 +0530

    Create User_balance.java

---
+++ TASK3 /User_balance.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package task3;
+
+public class UserBank {
+   public double balance;
+ public void Balance(double initialBalance){
+     balance = initialBalance;
+ } 
+}
